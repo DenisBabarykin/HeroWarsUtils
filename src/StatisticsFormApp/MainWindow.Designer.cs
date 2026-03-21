9e53fb85e583df1addccad2a6a8b207bff7cbf3b
Denis Babarykin <babarykin.denis@gmail.com>
2026-03-21 19:40:22 +0300

    GUI drafted.

--- src/StatisticsFormApp/MainWindow.Designer.cs
+++ src/StatisticsFormApp/MainWindow.Designer.cs
@@ -29,19 +29,42 @@
         private void InitializeComponent()
         {
             menuStrip = new MenuStrip();
+            ToolStripMenuItemFile = new ToolStripMenuItem();
+            ToolStripMenuItemExportExcel = new ToolStripMenuItem();
             ToolStripMenuItemParams = new ToolStripMenuItem();
+            groupBoxActivity = new GroupBox();
+            textBoxActivity = new TextBox();
+            groupBoxTitanit = new GroupBox();
+            textBoxTitanit = new TextBox();
             menuStrip.SuspendLayout();
+            groupBoxActivity.SuspendLayout();
+            groupBoxTitanit.SuspendLayout();
             SuspendLayout();
             // 
             // menuStrip
             // 
             menuStrip.ImageScalingSize = new Size(24, 24);
-            menuStrip.Items.AddRange(new ToolStripItem[] { ToolStripMenuItemParams });
+            menuStrip.Items.AddRange(new ToolStripItem[] { ToolStripMenuItemFile, ToolStripMenuItemParams });
             menuStrip.Location = new Point(0, 0);
             menuStrip.Name = "menuStrip";
-            menuStrip.Size = new Size(1232, 33);
+            menuStrip.Size = new Size(1027, 33);
             menuStrip.TabIndex = 3;
             menuStrip.Text = "menuStrip1";
+            // 
+            // ToolStripMenuItemFile
+            // 
+            ToolStripMenuItemFile.DropDownItems.AddRange(new ToolStripItem[] { ToolStripMenuItemExportExcel });
+            ToolStripMenuItemFile.Name = "ToolStripMenuItemFile";
+            ToolStripMenuItemFile.Size = new Size(69, 29);
+            ToolStripMenuItemFile.Text = "Файл";
+            // 
+            // ToolStripMenuItemExportExcel
+            // 
+            ToolStripMenuItemExportExcel.Enabled = false;
+            ToolStripMenuItemExportExcel.Name = "ToolStripMenuItemExportExcel";
+            ToolStripMenuItemExportExcel.Size = new Size(239, 34);
+            ToolStripMenuItemExportExcel.Text = "Экспорт в Excel";
+            ToolStripMenuItemExportExcel.Click += ToolStripMenuItemExportExcel_Click;
             // 
             // ToolStripMenuItemParams
             // 
@@ -50,18 +73,67 @@
             ToolStripMenuItemParams.Text = "Параметры";
             ToolStripMenuItemParams.Click += ToolStripMenuItemParams_Click;
             // 
+            // groupBoxActivity
+            // 
+            groupBoxActivity.Controls.Add(textBoxActivity);
+            groupBoxActivity.Location = new Point(12, 36);
+            groupBoxActivity.Name = "groupBoxActivity";
+            groupBoxActivity.Size = new Size(500, 752);
+            groupBoxActivity.TabIndex = 4;
+            groupBoxActivity.TabStop = false;
+            groupBoxActivity.Text = "Активность (csv)";
+            // 
+            // textBoxActivity
+            // 
+            textBoxActivity.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBoxActivity.Location = new Point(6, 30);
+            textBoxActivity.Multiline = true;
+            textBoxActivity.Name = "textBoxActivity";
+            textBoxActivity.ScrollBars = ScrollBars.Both;
+            textBoxActivity.Size = new Size(488, 716);
+            textBoxActivity.TabIndex = 0;
+            textBoxActivity.TextChanged += textBoxActivity_TextChanged;
+            // 
+            // groupBoxTitanit
+            // 
+            groupBoxTitanit.Controls.Add(textBoxTitanit);
+            groupBoxTitanit.Location = new Point(518, 36);
+            groupBoxTitanit.Name = "groupBoxTitanit";
+            groupBoxTitanit.Size = new Size(500, 752);
+            groupBoxTitanit.TabIndex = 5;
+            groupBoxTitanit.TabStop = false;
+            groupBoxTitanit.Text = "Титанит (csv)";
+            // 
+            // textBoxTitanit
+            // 
+            textBoxTitanit.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBoxTitanit.Location = new Point(6, 30);
+            textBoxTitanit.Multiline = true;
+            textBoxTitanit.Name = "textBoxTitanit";
+            textBoxTitanit.ScrollBars = ScrollBars.Both;
+            textBoxTitanit.Size = new Size(487, 716);
+            textBoxTitanit.TabIndex = 1;
+            textBoxTitanit.TextChanged += textBoxTitanit_TextChanged;
+            // 
             // MainWindow
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1232, 800);
+            ClientSize = new Size(1027, 800);
+            Controls.Add(groupBoxTitanit);
+            Controls.Add(groupBoxActivity);
             Controls.Add(menuStrip);
             MainMenuStrip = menuStrip;
             Margin = new Padding(4);
+            MaximizeBox = false;
             Name = "MainWindow";
             Text = "Менеджер статистики";
             menuStrip.ResumeLayout(false);
             menuStrip.PerformLayout();
+            groupBoxActivity.ResumeLayout(false);
+            groupBoxActivity.PerformLayout();
+            groupBoxTitanit.ResumeLayout(false);
+            groupBoxTitanit.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -69,5 +141,11 @@
         #endregion
         private MenuStrip menuStrip;
         private ToolStripMenuItem ToolStripMenuItemParams;
+        private ToolStripMenuItem ToolStripMenuItemFile;
+        private ToolStripMenuItem ToolStripMenuItemExportExcel;
+        private GroupBox groupBoxActivity;
+        private TextBox textBoxActivity;
+        private GroupBox groupBoxTitanit;
+        private TextBox textBoxTitanit;
     }
 }
--- src/StatisticsFormApp/MainWindow.cs
+++ src/StatisticsFormApp/MainWindow.cs
@@ -1,16 +1,53 @@
-namespace StatisticsFormApp
-{
-    public partial class MainWindow : Form
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+namespace StatisticsFormApp;
 
-        private void ToolStripMenuItemParams_Click(object sender, EventArgs e)
+public partial class MainWindow : Form
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ToolStripMenuItemParams_Click(object sender, EventArgs e)
+    {
+        var paramsForm = new ParamsForm();
+        paramsForm.ShowDialog();
+    }
+
+    private void textBoxActivity_TextChanged(object sender, EventArgs e)
+    {
+        CheckExportAbility();
+    }
+
+    private void textBoxTitanit_TextChanged(object sender, EventArgs e)
+    {
+        CheckExportAbility();
+    }
+
+    private void ToolStripMenuItemExportExcel_Click(object sender, EventArgs e)
+    {
+        var dialog = new SaveFileDialog
         {
-            var paramsForm = new ParamsForm();
-            paramsForm.ShowDialog();
+            Filter = "Excel файлы (*.xlsx)|*.xlsx",
+            DefaultExt = "xlsx",
+            FileName = $"Смотр {DateTime.Now:dd.MM.yyyy}",
+            InitialDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads")
+        };
+
+        if (dialog.ShowDialog() == DialogResult.OK)
+        {
+            MessageBox.Show("Сохранено в: " + dialog.FileName);
+        }
+    }
+
+    private void CheckExportAbility()
+    {
+        if (string.IsNullOrWhiteSpace(textBoxActivity.Text) || string.IsNullOrWhiteSpace(textBoxTitanit.Text))
+        {
+            ToolStripMenuItemExportExcel.Enabled = false;
+        }
+        else
+        {
+            ToolStripMenuItemExportExcel.Enabled = true;
         }
     }
 }
